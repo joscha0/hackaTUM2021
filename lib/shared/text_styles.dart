f30ab1829d3dc5e48c503ee75c1e0bcf66b93f8b
Joscha von Andrian <joschavonandrian@gmail.com>
2021-11-20 00:34:07 +0100

    add text styles

---
+++ lib/shared/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const TextStyle heading = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 28,
+);
+
+const TextStyle smallHeading = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 21,
+);
