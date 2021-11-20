6281a051f946b239cad9c1473a1b061cceb219c4
Joscha von Andrian <joschavonandrian@gmail.com>
2021-11-21 00:22:04 +0100

    add list builder

--- lib/modules/projects/projects_view.dart
+++ lib/modules/projects/projects_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:uplant/modules/projects/project_view.dart';
 import 'package:uplant/modules/projects/projects_controller.dart';
 import 'package:uplant/shared/bottom_nav_bar.dart';
+import 'package:uplant/shared/content_card.dart';
 import 'package:uplant/shared/text_styles.dart';
 import 'package:get/get.dart';
+
+import 'add_project_view.dart';
 
 class ProjectsView extends GetView<ProjectsController> {
   const ProjectsView({Key? key}) : super(key: key);
@@ -18,12 +22,33 @@ class ProjectsView extends GetView<ProjectsController> {
           style: heading,
         ),
       ),
-      body: SafeArea(child: Text('ProjectsController')),
+      body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: GetX<ProjectsController>(
+              init: Get.put<ProjectsController>(ProjectsController()),
+              builder: (ProjectsController projectsController) {
+                return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: projectsController.projects.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final project = projectsController.projects[index];
+                      return ContentCard(
+                        onPressed: () {
+                          Get.to(ProjectView(project: project));
+                        },
+                        title: project.title,
+                        infoText: project.infoText,
+                        imageURL: project.imageURL,
+                      );
+                    });
+              })),
       bottomNavigationBar: MyBottomNavigationBar(
         currentIndex: 1,
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () {
+          Get.to(AddProjectView());
+        },
         label: const Text('add project'),
         icon: const Icon(Icons.add),
       ),
