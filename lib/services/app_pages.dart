375f5560c9b3e48af54ce089636055d871b744ea
Joscha von Andrian <joschavonandrian@gmail.com>
2021-11-20 01:59:24 +0100

    add bindings

--- lib/services/app_pages.dart
+++ lib/services/app_pages.dart
@@ -1,21 +1,23 @@
 import 'package:get/get.dart';
+import 'package:repair/modules/home/home_binding.dart';
 import 'package:repair/modules/home/home_view.dart';
+import 'package:repair/modules/login/login_binding.dart';
 import 'package:repair/modules/login/login_view.dart';
+import 'package:repair/modules/profile/profile_binding.dart';
 import 'package:repair/modules/profile/profile_view.dart';
+import 'package:repair/modules/settings/settings_binding.dart';
 import 'package:repair/modules/settings/settings_view.dart';
 
 import 'auth_middleware.dart';
 part 'app_routes.dart';
 
 class AppPages {
-  AppPages._();
-
+  static const INITIAL = Routes.HOME;
   static final routes = [
     GetPage(
       name: Routes.HOME,
       page: () => const HomeView(),
-      participatesInRootNavigator: true,
-      preventDuplicates: true,
+      binding: HomeBinding(),
       children: [
         GetPage(
           middlewares: [
@@ -23,14 +25,17 @@ class AppPages {
           ],
           name: Routes.LOGIN,
           page: () => const LoginView(),
+          binding: LoginBinding(),
         ),
         GetPage(
           name: Routes.PROFILE,
           page: () => const ProfileView(),
+          binding: ProfileBinding(),
         ),
         GetPage(
           name: Routes.SETTINGS,
           page: () => const SettingsView(),
+          binding: SettingsBinding(),
         ),
       ],
     ),
