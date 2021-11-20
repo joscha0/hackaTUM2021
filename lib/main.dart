ed83b254a9c92442ec7248e7b905a640fbb39625
Joscha von Andrian <joschavonandrian@gmail.com>
2021-11-20 21:55:12 +0100

    add firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uplant/modules/projects/projects_view.dart';
 import 'package:get/get.dart';
 
 import 'services/auth_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
