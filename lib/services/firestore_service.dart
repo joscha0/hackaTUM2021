7507e8e636cd267f1bca23831cdcda1a853a6e89
Joscha von Andrian <joschavonandrian@gmail.com>
2021-11-20 19:43:23 +0100

    add firestore

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,72 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uplant/models/guide_model.dart';
+import 'package:uplant/models/help_model.dart';
+import 'package:uplant/models/project_model.dart';
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+class FirebaseService {
+  static addProject(ProjectModel project) async {
+    await firestore.collection("projects").add({
+      'title': project.title,
+      'createdOn': Timestamp.now(),
+      'ownerName': project.ownerName,
+      'infoText': project.infoText,
+      'imageUrl': project.imageURL,
+      'comments': project.comments
+    });
+  }
+
+  static addHelp(HelpModel help) async {
+    await firestore.collection("help").add({
+      'title': help.title,
+      'createdOn': Timestamp.now(),
+      'ownerName': help.ownerName,
+      'infoText': help.infoText,
+      'imageUrl': help.imageURL,
+      'comments': help.comments
+    });
+  }
+
+  static Stream<List<ProjectModel>> projectStrem() {
+    return firestore
+        .collection("projects")
+        .snapshots()
+        .map((QuerySnapshot query) {
+      List<ProjectModel> projects = [];
+      for (var project in query.docs) {
+        final projectModel =
+            ProjectModel.fromDocumentSnapshot(documentSnapshot: project);
+        projects.add(projectModel);
+      }
+      return projects;
+    });
+  }
+
+  static Stream<List<HelpModel>> helpStrem() {
+    return firestore.collection("help").snapshots().map((QuerySnapshot query) {
+      List<HelpModel> helps = [];
+      for (var help in query.docs) {
+        final helpModel =
+            HelpModel.fromDocumentSnapshot(documentSnapshot: help);
+        helps.add(helpModel);
+      }
+      return helps;
+    });
+  }
+
+  static Stream<List<GuideModel>> guidesStrem() {
+    return firestore
+        .collection("guides")
+        .snapshots()
+        .map((QuerySnapshot query) {
+      List<GuideModel> guides = [];
+      for (var guide in query.docs) {
+        final guideModel =
+            GuideModel.fromDocumentSnapshot(documentSnapshot: guide);
+        guides.add(guideModel);
+      }
+      return guides;
+    });
+  }
+}
